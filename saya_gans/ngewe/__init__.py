ff3362ad5e479227074ffd1cd41b765b0c3efae7
ROY-OFFICIAL <85698618+ROY-OFFICIAL@users.noreply.github.com>
2021-06-11 16:59:09 +0700

    Update __init__.py

--- saya_gans/ngewe/__init__.py
+++ saya_gans/ngewe/__init__.py
@@ -26,7 +26,7 @@ class gadag_user:
 			for __ in _:
 				___=re.search("id=(\d*)" if "profile.php" in __[0] else "(.*?)\?",__[0])
 				romiafrzl.append(___.group(1)+"(Romi Afrizal)"+__[1] if ___ is not None else __[0]+"(Romi Afrizal)"+__[1])
-				print(f"\r\x1b[1;37m [*] Sedang dump {len(romiafrzl)} mohon tunggu!",end="")
+				print(f"\r\x1b[1;37m ["+U+"*"+P+"] Sedang dump {len(romiafrzl)} mohon tunggu!",end="")
 			if "Lihat Selengkapnya" in link:
 				self.followers(self.url+parser(link,"html.parser").find("a",string="Lihat Selengkapnya")["href"],True)
 			return romiafrzl
