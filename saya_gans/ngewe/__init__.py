88fb4ff1a2663a1c9ba4d223b45d84f4bb455f6c
ROY-OFFICIAL <85698618+ROY-OFFICIAL@users.noreply.github.com>
2021-06-11 17:00:53 +0700

    Update __init__.py

--- saya_gans/ngewe/__init__.py
+++ saya_gans/ngewe/__init__.py
@@ -39,7 +39,7 @@ class gadag_user:
 			_=re.findall('style\=\"vertical-align: middle"\>\<a\ class\=\"..\" href\=\"/(.*?)"\>(.*?)</a\>',link)
 			for __ in _:
 				romiafrzl.append(re.search("id=(\d*)" if "profile.php" in __ [0] else "(.*?)\?",__[0]).group(1)+"(Romi Afrizal)"+__[1])
-				print(f"\r\x1b[1;37m [*] Sedang dump {len(romiafrzl)} mohon tunggu!",end="")
+				print(f"\r\x1b[1;37m ["+U+"*"+p+"] Sedang dump {len(romiafrzl)} mohon tunggu!",end="")
 			if "Lihat Teman Lain" in link:
 				self.fl(self.url+parser(link,"html.parser").find("a",string="Lihat Teman Lain")["href"],True)
 			return romiafrzl
